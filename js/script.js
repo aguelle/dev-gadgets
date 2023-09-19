587ac97025b31122c48c053fef4f916f4f35438d
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-09-19 11:02:15 +0200

    modify html to display img size L

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 //1. Galerie des photos (mobile)
 
+// document.querySelector("#pictures .pictures-prev").addEventListener("click", function () {
+//     let imgList = document.querySelector("#gallery-thumbs");
+//     imgList.appendChild(imgList.firstElementChild);
+//   });
+//   document.querySelector("#pictures .button .pictures-next").addEventListener("click", function () {
+//     let imgList = document.querySelector("#gallery-thumbs");
+//     imgList.appendChild(imgList.lastElementChild);
+//   });
 //2. Galerie des photos (desktop)
+
 document.querySelectorAll(".thumbs-img").forEach((img) => {
   document.querySelectorAll("#gallery-thumbs img[data-title]").forEach((img) => {
       img.addEventListener("mouseover", function () {
@@ -15,6 +24,7 @@ document.querySelectorAll(".thumbs-img").forEach((img) => {
 function showImage(imageData) {
   document.getElementById("pictures-img").src = imageData.src;
 }
+
 //3. Ajout au panier
 
 //4. Accordéons
